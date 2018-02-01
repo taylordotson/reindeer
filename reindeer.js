f7f9e1e58c9cd6ac5cf53d13c5859b326751fbc0
taylordotson <33382665+taylordotson@users.noreply.github.com>
2018-02-01 15:54:01 -0600

    first commit

---
+++ reindeer.js
@@ -0,0 +1,10 @@
+
+
+
+
+function myFunction() {
+    var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+    var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+    var pleaseWork = colors.concat(reindeer);
+    document.getElementById("reindeer").innerHTML = pleaseWork;
+}
