d253bf22504e518ca05d5990f67207ae424948e6
taylordotson <33382665+taylordotson@users.noreply.github.com>
2018-02-27 13:27:01 -0600

    added a loop

--- reindeer.js
+++ reindeer.js
@@ -1,10 +1,9 @@
 
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
-
-
-function myFunction() {
-    var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
-    var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
-    var pleaseWork = colors.concat(reindeer);
-    document.getElementById("reindeer").innerHTML = pleaseWork;
+for (var i = 0; i < reindeer.length; i++) {
+    var reindeerColor = colors[i] + " " + reindeer[i] + "<br>";
+    var hohohoElement = document.getElementById("reindeer");
+    hohohoElement.innerHTML += reindeerColor;
 }
