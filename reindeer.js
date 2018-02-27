01be188a0880319e44cb71b4aa6595103179e185
taylordotson <33382665+taylordotson@users.noreply.github.com>
2018-02-27 14:05:02 -0600

    up and running

--- reindeer.js
+++ reindeer.js
@@ -1,9 +1,11 @@
-
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+var reindeerLength = reindeer.length;
+var reindeerColor = "";
 
-for (var i = 0; i < reindeer.length; i++) {
-    var reindeerColor = colors[i] + " " + reindeer[i] + "<br>";
-    var hohohoElement = document.getElementById("reindeer");
-    hohohoElement.innerHTML += reindeerColor;
+for (var i = 0; i < reindeerLength; i++) {
+    reindeerColor += colors[i] + " " + reindeer[i] + "<br>";
 }
+
+var hohohoElement = document.getElementById("reindeer");
+document.getElementById("reindeer").innerHTML = reindeerColor;
